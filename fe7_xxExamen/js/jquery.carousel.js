9620e2ed38234498e510017de2b23b01d6e333b1
ngineus <ngineus@gmail.com>
2017-03-28 21:58:54 +0300

    Second Try

--- fe7_xxExamen/js/jquery.carousel.js
+++ fe7_xxExamen/js/jquery.carousel.js
@@ -22,4 +22,8 @@ $(function() {
             .jcarouselControl({
                 target: '+=1'
             });
-          });
+
+
+
+
+    });
--- fe7_xxExamen/js/script.js
+++ fe7_xxExamen/js/script.js
@@ -1,13 +1,40 @@
-﻿$(function() { // jQuery Запускаем по окончании загрузки страницы
-  // $('carousel-list').carouselka();
+// ﻿$(function() { // jQuery Запускаем по окончании загрузки страницы
+//   // $('carousel-list').carouselka();
+//
+//   $.fn.carouselka = function() { // К стандартному функционалу джиквери добавляем свой функционал
+//
+//   }
+// });
+//
+// (function($) { // jQuery плагин
+//
+//
+//   return this;
+// })(jQuery);
 
-  $.fn.carouselka = function() { // К стандартному функционалу джиквери добавляем свой функционал
-    
-  }
-});
 
-(function($) { // jQuery плагин
+$(function() {
+        $('.jcarousel').jcarousel();
 
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
 
-  return this;
-})(jQuery);
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+          });
